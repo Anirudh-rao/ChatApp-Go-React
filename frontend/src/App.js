4a7e6dd7576be65cf5d5ab68103553adba9b1d9a
Anirudh Rao <anirudh131299@gmail.com>
2022-05-29 11:11:04 +0530

    Added Styled Header Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component }  from 'react';
 import './App.css';
 import {connect, sendMsg}  from './api';
+import Header from './components/Header';
 
 class App extends Component{
   constructor(props) {
@@ -14,6 +15,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
+        <Header />
         <button onClick={this.send}>Hit</button>
       </div>
     )
